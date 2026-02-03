d4d6c6da27fb4aa4e3d9266b482df927cc0d986c
Nabiel Utomo <nabielutomo@gmail.com>
2026-02-03 13:29:47 +0700

    Add add_review view for posting dealer reviews with authentication check

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -14,7 +14,7 @@ import json
 from django.views.decorators.csrf import csrf_exempt
 from .populate import initiate
 from .models import CarMake, CarModel
-from .restapis import get_request, analyze_review_sentiments
+from .restapis import get_request, analyze_review_sentiments, post_review
 
 
 # Get an instance of a logger
@@ -91,7 +91,17 @@ def get_dealer_reviews(request, dealer_id):
     return JsonResponse({"status":200,"reviews":reviews})
 
 # Create a `add_review` view to submit a review
-# def add_review(request):
+@csrf_exempt
+def add_review(request):
+    if(request.user.is_anonymous == False):
+        data = json.loads(request.body)
+        try:
+            response = post_review(data)
+            return JsonResponse({"status":200})
+        except:
+            return JsonResponse({"status":401,"message":"Error in posting review"})
+    else:
+        return JsonResponse({"status":403,"message":"Unauthorized"})
 
 
 def get_cars(request):
